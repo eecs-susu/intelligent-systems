2f353efd0eb1a9e8f3304bb6379dc7f4ff0c0d71
alkapov <ralkapov@gmail.com>
2018-10-20 23:25:21 +0500

    add triangles

--- Task 1/image-generator.py
+++ Task 1/image-generator.py
@@ -5,10 +5,12 @@ from random import Random
 import cv2
 import numpy as np
 
+COLOR_WHITE = (255, 255, 255)
 
-def generate_point(width, height, random):
-    x = np.float32(random.uniform(0, width))
-    y = np.float32(random.uniform(0, height))
+
+def generate_point(width, height, random, padding=0):
+    x = np.float32(random.uniform(5, width - padding))
+    y = np.float32(random.uniform(5, height - padding))
     return x, y
 
 
@@ -38,7 +40,8 @@ def generate_circles(width, height, count=1, seed=None):
     for i in range(count):
         image = source.copy()
         x, y, r = generate_circle_coords(width, height, random)
-        yield cv2.circle(image, (x, y), r, (255, 255, 255))
+        yield cv2.circle(image, (x, y), r, COLOR_WHITE,
+                         lineType=cv2.LINE_AA)
 
 
 def generate_ellipses(width, height, count=1, seed=None):
@@ -52,8 +55,8 @@ def generate_ellipses(width, height, count=1, seed=None):
         image = source.copy()
         x, y, ra, rb = generate_ellipse_coords(width, height, random)
         angle = random.randint(0, 360)
-        color = (255, 255, 255)
-        yield cv2.ellipse(image, (x, y), (ra, rb), angle, 0, 360, color)
+        yield cv2.ellipse(image, (x, y), (ra, rb), angle, 0, 360, COLOR_WHITE,
+                          lineType=cv2.LINE_AA)
 
 
 def generate_broken_lines(width, height, count=1, seed=None):
@@ -68,8 +71,9 @@ def generate_broken_lines(width, height, count=1, seed=None):
         start = generate_point(width, height, random)
         mid = generate_point(width, height, random)
         end = generate_point(width, height, random)
-        image = cv2.line(image, start, mid, (255, 255, 255))
-        yield cv2.line(image, mid, end, (255, 255, 255))
+        image = cv2.line(image, start, mid, COLOR_WHITE)
+        yield cv2.line(image, mid, end, COLOR_WHITE,
+                       lineType=cv2.LINE_AA)
 
 
 def generate_lines(width, height, count=1, seed=None):
@@ -83,7 +87,87 @@ def generate_lines(width, height, count=1, seed=None):
         image = source.copy()
         start = generate_point(width, height, random)
         end = generate_point(width, height, random)
-        yield cv2.line(image, start, end, (255, 255, 255))
+        yield cv2.line(image, start, end, COLOR_WHITE,
+                       lineType=cv2.LINE_AA)
+
+
+def generate_rectangles(width, height, count=1, seed=None):
+    random = Random()
+    random.seed(seed)
+
+    matrix = np.zeros((height, width), np.float32)
+    source = cv2.cvtColor(matrix, cv2.COLOR_GRAY2BGR)
+
+    for i in range(count):
+        image = source.copy()
+        start = generate_point(width, height, random)
+        end = generate_point(width, height, random)
+        yield cv2.rectangle(image, start, end, COLOR_WHITE,
+                            lineType=cv2.LINE_AA)
+
+
+def generate_squares(width, height, count=1, seed=None):
+    random = Random()
+    random.seed(seed)
+
+    matrix = np.zeros((height, width), np.float32)
+    source = cv2.cvtColor(matrix, cv2.COLOR_GRAY2BGR)
+
+    for i in range(count):
+        image = source.copy()
+        dist = np.float32(random.randint(5, min(width, height)))
+        start = generate_point(width, height, random, dist + 5)
+        x, y = start
+        yield cv2.rectangle(image, start, (x + dist, y + dist), COLOR_WHITE,
+                            lineType=cv2.LINE_AA)
+
+
+def generate_right_triangles(width, height, count=1, seed=None):
+    random = Random()
+    random.seed(seed)
+
+    matrix = np.zeros((height, width), np.float32)
+    source = cv2.cvtColor(matrix, cv2.COLOR_GRAY2BGR)
+
+    for i in range(count):
+        image = source.copy()
+        x2, y2 = -1, -1
+        x0, y0, x1, y1 = 0., 0., 0., 0.
+        while not (0 <= x2 < width and 0 <= y2 < height):
+            x0, y0 = generate_point(width, height, random)
+            x1, y1 = generate_point(width, height, random)
+            dx, dy = x1 - x0, y1 - y0
+            x2 = np.float32(x0 + dy * random.random())
+            y2 = np.float32(y0 - dx * random.random())
+        image = cv2.line(image, (x0, y0), (x1, y1), COLOR_WHITE)
+        image = cv2.line(image, (x2, y2), (x1, y1), COLOR_WHITE)
+        yield cv2.line(image, (x0, y0), (x2, y2), COLOR_WHITE,
+                       lineType=cv2.LINE_AA)
+
+
+def generate_isosceles_triangles(width, height, count=1, seed=None):
+    random = Random()
+    random.seed(seed)
+
+    matrix = np.zeros((height, width), np.float32)
+    source = cv2.cvtColor(matrix, cv2.COLOR_GRAY2BGR)
+
+    for i in range(count):
+        image = source.copy()
+        x2, y2 = -1, -1
+        x0, y0, x1, y1 = 0., 0., 0., 0.
+        while not (0 <= x2 < width and 0 <= y2 < height):
+            x0, y0 = generate_point(width, height, random)
+            x1, y1 = generate_point(width, height, random)
+            dx, dy = x1 - x0, y1 - y0
+            xM, yM = (x0 + x1) / 2, (y0 + y1) / 2
+            mult = random.random()
+            x2 = np.float32(xM + dy)
+            y2 = np.float32(yM - dx)
+        image = cv2.line(image, (x0, y0), (x1, y1), COLOR_WHITE)
+        image = cv2.line(image, (x2, y2), (x1, y1), COLOR_WHITE)
+        yield cv2.line(image, (x0, y0), (x2, y2), COLOR_WHITE,
+                       lineType=cv2.LINE_AA)
 
 
 def save_shapes(path, generator):
@@ -105,11 +189,15 @@ def main():
         ('ellipses', generate_ellipses),
         ('lines', generate_lines),
         ('broken-lines', generate_broken_lines),
+        ('rectangles', generate_rectangles),
+        ('squares', generate_squares),
+        ('right-triangles', generate_right_triangles),
+        ('isosceles_triangles', generate_isosceles_triangles),
     ]
 
     for sub_path, generator in shapes:
         save_shapes(os.path.join(path, sub_path),
-                    generator(width, heigth, samples_per_type, 100))
+                    generator(width, heigth, samples_per_type, 200))
 
 
 if __name__ == '__main__':
